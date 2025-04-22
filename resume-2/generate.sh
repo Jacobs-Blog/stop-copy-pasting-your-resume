5f61fb6359757e368a3b2566909f448e3d441e6f
Jacob Duijzer <jacob@duijzer.com>
2025-04-22 10:59:21 +0200

    Correct working dir in script

--- resume-2/generate.sh
+++ resume-2/generate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$SCRIPT_DIR" || exit 1
+
 asciidoctor-pdf --out-file=one-pager-john-doe.pdf \
     --backend=pdf \
     --attribute doctype=article \
